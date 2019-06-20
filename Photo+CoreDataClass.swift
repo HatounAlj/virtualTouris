ed01de947e2f12397403c1e98c28783abf1cdc3a
Hatounalj2 <436204141@student.ksu.edu.sa>
2019-06-20 11:06:30 +0300

    nn

---
+++ Photo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Photo+CoreDataClass.swift
+//  Touris
+//
+//  Created by Eng.Hatoun 👩🏻‍💻 on 15/10/1440 AH.
+//  Copyright © 1440 Eng.Hatoun 👩🏻‍💻. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
